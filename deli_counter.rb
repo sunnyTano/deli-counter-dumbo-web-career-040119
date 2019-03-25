1be5aea150acbf9e71ca8d5b6ca56a599bb45c76
Susan Tan <ssusantan9@gmail.com>
2019-03-25 02:29:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
       line_positions << "#{number}. #{name}"
       number += 1
     end
-    puts "The line is currently: #{line_positions.join(" ")}"
+    puts "The line is currently: #{current.join(" ")}"
   end
 end
   
