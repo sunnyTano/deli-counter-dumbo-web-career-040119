f9a9710ef392291a187223600e0533a804f7ed09
Susan Tan <ssusantan9@gmail.com>
2019-03-25 02:47:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,13 @@ def take_a_number(katz_deli, str)
 end
   
 def now_serving(katz_deli)
-  
+  if katz_deli.length == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
 
 #   describe "#now_serving" do
 #     context "there are no people in line" do
